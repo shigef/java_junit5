bdd7af75018f51e5c2fb751e67f64949574ee01d
shigef <shigerhum@gmail.com>
2021-03-06 11:06:30 +0900

    いちごのテストを作成

---
+++ src/test/java/tddbc/StrawberryTest.java
@@ -0,0 +1,20 @@
+package tddbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StrawberryTest {
+
+    @Test
+    public void いちご_品種_あまおうとサイズ_S_からサイズ_S_が取得できること() throws Exception {
+        // Setup
+        Strawberry sut = new Strawberry("あまおう", "S");
+
+        // Exercise
+        String actual = sut.size;
+        // Verify
+        assertEquals("S", actual);
+    }
+
+}
