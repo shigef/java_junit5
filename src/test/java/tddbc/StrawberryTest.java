78f66df802eb5bb91e026d70c1136dbf4494e86b
shigef <shigerhum@gmail.com>
2021-05-29 11:23:44 +0900

    REFACTOR: 品種と重さ指定のテストのDisplayNameを修正

--- src/test/java/tddbc/StrawberryTest.java
+++ src/test/java/tddbc/StrawberryTest.java
@@ -119,7 +119,7 @@ public class StrawberryTest {
 		@DisplayName("いちごから品種が取得できる")
 		public class いちごから品種が取得できる {
 			@Test
-			@DisplayName("いちご（品種：あまおうと重さ： 1.0g）から品種のいちごが取得できること")
+			@DisplayName("いちご（品種：あまおうと重さ： 1.0g）から品種(あまおう)のいちごが取得できること")
 			public void いちご_品種_あまおうと重さ_1_0_から品種_あまおう_が取得できること() throws Exception {
 				// Setup
 				Strawberry sut = new Strawberry(Strawberry.Varieties.AMAOU, new BigDecimal("1.0"));
