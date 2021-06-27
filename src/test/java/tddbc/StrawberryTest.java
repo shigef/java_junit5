7d7aeaebfedcccd914933c0e247588849afa04bd
きだ <vox37ster@gmail.com>
2021-06-27 10:37:04 +0900

    REFACTOR:Strawberryの「品種とサイズを指定したいちごが取得できること」テストケースから不要なテストケースをコメントアウトする。

--- src/test/java/tddbc/StrawberryTest.java
+++ src/test/java/tddbc/StrawberryTest.java
@@ -44,27 +44,27 @@ public class StrawberryTest {
 			// Verify
 			assertEquals("あまおう", actual);
 		}
-		@Test
-		@DisplayName("いちご（品種：あまおうとサイズ：S）からサイズ（S）が取得できること")
-		public void いちご_品種_あまおうとサイズ_S_からサイズ_S_が取得できること() throws Exception {
-			// Setup
-			Strawberry sut = new Strawberry(Strawberry.Varieties.AMAOU, "S");
-			// Exercise
-			String actual = sut.getSize();
-			// Verify
-			assertEquals("S", actual);
-		}
+//		@Test
+//		@DisplayName("いちご（品種：あまおうとサイズ：S）からサイズ（S）が取得できること")
+//		public void いちご_品種_あまおうとサイズ_S_からサイズ_S_が取得できること() throws Exception {
+//			// Setup
+//			Strawberry sut = new Strawberry(Strawberry.Varieties.AMAOU, "S");
+//			// Exercise
+//			String actual = sut.getSize();
+//			// Verify
+//			assertEquals("S", actual);
+//		}
 
-		@Test
-		@DisplayName("いちご（品種：あまおうとサイズ：S）から品種（あまおう）が取得できること")
-		public void いちご_品種_あまおうとサイズ_S_から品種_あまおう_が取得できること() throws Exception {
-			// Setup
-			Strawberry sut = new Strawberry(Strawberry.Varieties.AMAOU, "S");
-			// Exercise
-			String actual = sut.getVariety();
-			// Verify
-			assertEquals("あまおう", actual);
-		}
+//		@Test
+//		@DisplayName("いちご（品種：あまおうとサイズ：S）から品種（あまおう）が取得できること")
+//		public void いちご_品種_あまおうとサイズ_S_から品種_あまおう_が取得できること() throws Exception {
+//			// Setup
+//			Strawberry sut = new Strawberry(Strawberry.Varieties.AMAOU, "S");
+//			// Exercise
+//			String actual = sut.getVariety();
+//			// Verify
+//			assertEquals("あまおう", actual);
+//		}
 	}
 
 	@Nested
